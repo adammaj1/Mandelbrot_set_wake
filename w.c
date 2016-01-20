01da2b3ae8c282f011837ed00a837f37ef23e6ec
Adam Majewski <adammaj1@o2.pl>
2016-01-20 13:53:08 +0100

    all in gmp code

--- w.c
+++ w.c
@@ -77,6 +77,7 @@ backwards for some reason ..."
 in Douady angles ... or if no one has written it down,  certainly many people
 have found it by drawing the petals and numbering them ..."
 
+Comments by Wolf Jung
 
  
 
@@ -142,7 +143,7 @@ unsigned long long int GiveNum(unsigned long long int n, unsigned long long int 
 {  
    unsigned long long int num = 1LL;
    unsigned long long int  f = 1LL; 
-   unsigned long long int j, s = 0; 
+   long long int j, s = 0; 
 
 
       
@@ -157,60 +158,119 @@ unsigned long long int GiveNum(unsigned long long int n, unsigned long long int 
    
 }
 
-void mpq_wake_str(mpq_t rop, char *str, int base )
-{
-   mpq_t q;   // internal angle = rational number q = n/d
-  // arbitrary precision variables from GMP library
-   mpz_t  n ; // numerator of q
-   mpz_t  d ; // denominator of q
 
+ // rotation map 
+//the number  n  is always increased by n0 modulo d
+
+// input :  op = n/d ( rational number ) and n0 ( integer)
+//  n = (n + n0 ) % d
+// d = d
+// output = rop = n/d
+void mpq_rotation(mpq_t rop, const mpq_t op, const mpz_t n0)
+{
+  
+  mpz_t n; // numerator
+  mpz_t d; // denominator
+  mpz_inits( n, d, NULL);
+
+ 
+  //  
+  mpq_get_num (n, op); // 
+  mpq_get_den (d, op);
+  
+ 
+  // n = (n + n0 ) % d
+  mpz_add(n, n, n0); 
+  mpz_mod( n, n, d);
+  
+      
+  // output
+  mpq_set_num(rop, n);
+  mpq_set_den(rop, d);
+    
+  mpz_clears( n, d, NULL);
+
+
+}
+
+
+void mpq_wake(mpq_t rop, mpq_t op)
+{
+   
+  // arbitrary precision variables from GMP library
+   mpz_t  n0 ; // numerator of q
+   mpz_t  nc;
+   mpz_t  n;
+   mpz_t  d ; // denominator of q
+   mpz_t  m; // 2^i
 
    mpz_t  num ; // numerator of rop
    mpz_t  den ; // denominator of rop
+   long long int i;
+   unsigned long int base = 2;
    unsigned long int id;
-   unsigned long int in;
-   unsigned long int inum;
-   unsigned long int b = 2;
+   int cmp; 
+
+   mpz_inits(n, n0,nc,d,num,den,m, NULL);  
 
 
-   mpz_inits(n,d,num,den,NULL);  
-
-   // q
-   mpq_init (q); //
-   mpq_set_str(q, str, base);
-  
-   mpq_get_num(n,q);
-   mpq_get_den(d,q);
-   //  if (n <= 0 || n >= n ) error !!!!
-   mpq_canonicalize (q); // It is the responsibility of the user to canonicalize the assigned variable before any arithmetic operations are performed on that variable. 
+   mpq_get_num(n0,op);
+   mpq_get_den(d,op);
    id = mpz_get_ui(d);
-   in = mpz_get_ui(n);
-   
+   //  if (n <= 0 || n >= d ) error !!!! bad input
+   mpz_sub(nc, d, n0); // nc = d - n0
+   mpz_set(n, n0);   
+   mpz_set_ui(num, 0);
+  
 
-   
 
-
-    // rop  
+   // rop  
     // num = numerator(rop)
-    inum = GiveNum(in, id);
-    mpz_set_ui(num, inum);
+    
+   
    // denominator = den(rop) = (2^i) -1 
-   mpz_ui_pow_ui(den, b, id) ;  // den = b^i
+   mpz_ui_pow_ui(den, base, id) ;  // den = base^id
    mpz_sub_ui(den, den, 1);   // den = den-1
+   
+  // numerator   
+     for (i=0; i<id ; i++){  
+       
+       mpz_set_ui(m, 0);
+       cmp = mpz_cmp(n,nc);// Compare op1 and op2. Return a positive value if op1 > op2, zero if op1 = op2, or a negative value if op1 < op2.
+       if ( cmp>0 ) {
+          mpz_ui_pow_ui(m, 2, id-i-1); // m = 2^(id-i   )
+          mpz_add(num, num, m); // num = num + m
+          if (mpz_cmp(num, den) >0) mpz_mod( num, num, den); // num = num % d ; if num==d gives 0
+          gmp_printf("s = 1");
+
+           }
+         else gmp_printf("s = 0");
+       gmp_printf (" i = %ld internal angle = %Zd / %Zd ea = %Zd / %Zd ; m = %Zd \n", i, n, d, num, den, m);    
+
+        // n = (n + n0 ) % d = rotation 
+       mpz_add(n, n, n0); 
+       if (mpz_cmp(n, d)>0) mpz_mod( n, n, d);
+       //
+       
+          
+        // 
+      }
+
+   
+
+
+    
    // rop = external angle 
    mpq_set_num(rop,num);
    mpq_set_den(rop,den);
    mpq_canonicalize (rop); // It is the responsibility of the user to canonicalize the assigned variable before any arithmetic operations are performed on that variable. 
 
 
-   //
-   gmp_printf ("internal angle = %Qd\n",q); // 
-   gmp_printf ("external angle = %Qd\n",rop); // 
+   
 
     
    // clear memory
-   mpq_clear (q);
-   mpz_clears(n, d, num,den,  NULL);
+   mpz_clears(n, n0, nc, d, num,den, m, NULL);
 
 }
 
@@ -222,27 +282,39 @@ int main ()
         
         
        
-        
-        mpq_t q;   // rational number q = n/d
-        
-        
+        mpq_t  qExternalAngle;   // rational number q = n/d
+        char  *sExternalAngle;
 
-        // init and set variables 
-         mpq_init (q); //
-         mpq_wake_str(q,"4/5",10);   
-       
-        
-       
-        
+        int base = 10;
+        char *sIntenalAngle = "13/34";
+        mpq_t qInternalAngle;   // internal angle = rational number q = n/d
+        mpz_t den;  
+    
 
+        // init variables 
+        mpz_init(den);
+        mpq_inits (qExternalAngle, qInternalAngle, NULL); //
 
-         
-       
+        // set variables
+        mpq_set_str(qInternalAngle, sIntenalAngle, base); // string is an internal angle
+        mpq_canonicalize (qInternalAngle); // It is the responsibility of the user to canonicalize the assigned variable before any arithmetic operations are performed on that variable.
+        mpq_get_den(den,qInternalAngle); 
         
+        mpq_wake(qExternalAngle, qInternalAngle); // internal -> external    
+       // convertions
+        sExternalAngle = mpq_get_str (NULL, 2, qExternalAngle); // rational number = fraction : from decimal to binary
+        
+        
+        // print
+        gmp_printf ("internal angle = %Qd\n", qInternalAngle); // 
+        gmp_printf ("period = denominator of internal angle = %Zd\n", den); // 
+        gmp_printf ("external angle as a decimal fraction = %Qd\n", qExternalAngle); // 
+        gmp_printf ("external angle as a binary rational (string) : %s \n", sExternalAngle); // 
+   
         
         // clear memory
-        mpq_clear (q);
-        
+        mpq_clears(qExternalAngle, qInternalAngle, NULL);
+        mpz_clear(den);
         
         
         return 0;
